78a78205ce72273549578a0892605bab3fca5f02
Alexander Kim <akim3235@gmail.com>
2020-09-25 21:47:06 -0700

    Added parametrized "launch" query, rockets, and rocket.

--- schema.js
+++ schema.js
@@ -34,6 +34,33 @@ const RootQuery = new GraphQLObjectType({
         return axios.get('https://api.spacexdata.com/v3/launches')
         .then(res => res.data);      
       }
+    },
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+        .then(res => res.data)
+      }
+    },
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve(parent, args) { 
+        return axios.get('https://api.spacexdata.com/v3/rockets')
+        .then(res => res.data);      
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        return axios.get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+        .then(res => res.data)
+      }
     }
   }
 })
